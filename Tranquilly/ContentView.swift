b90064f52630ce79db2cac9649264344f6a7701a
Alex Bhandari <alexbhandari@Alexs-MacBook-Pro.local>
2023-12-30 19:39:05 -0800

    Initial commit

---
+++ Tranquilly/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Tranquilly
+//
+//  Created by Alex Bhandari on 12/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("START OF A NEW APP")
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tranquilly/TranquillyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranquillyApp.swift
+//  Tranquilly
+//
+//  Created by Alex Bhandari on 12/26/23.
+//
+
+import SwiftUI
+
+@main
+struct TranquillyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
